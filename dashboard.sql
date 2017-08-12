2cf59bc63dc4378d651187f4cf3b53e046dce0ba
Manny Perez <threedyou@gmail.com>
2017-08-12 15:55:11 -0500

    Mod to sql file

--- dashboard.sql
+++ dashboard.sql
@@ -1,23 +1,4 @@
 
---
--- Table structure for table `comments`
---
-
-DROP TABLE IF EXISTS `comments`;
-CREATE TABLE `comments` (
-  `id` int(11) NOT NULL auto_increment,
-  `photograph_id` int(11) NOT NULL,
-  `created` datetime NOT NULL,
-  `author` varchar(255) NOT NULL,
-  `body` text NOT NULL,
-  PRIMARY KEY  (`id`),
-  KEY `photograph_id` (`photograph_id`)
-) AUTO_INCREMENT=7;
-
---
--- Dumping data for table `comments`
---
-INSERT INTO `comments` VALUES (1,1,'2009-01-01 11:30:39','Manny','test 1'),(5,5,'2009-01-01 20:46:39','Bill','test 2'),(6,5,'2009-01-01 21:08:58','Jacob','test 3.');
 
 --
 -- Table structure for table `photographs`
@@ -36,7 +17,7 @@ CREATE TABLE `photographs` (
 --
 -- Dumping data for table `photographs`
 --
-INSERT INTO `photographs` VALUES (1,'bamboo.jpg','image/jpeg',265437,'Bamboo'),(5,'flowers.jpg','image/jpeg',394552,'Flowers'),(4,'roof.jpg','image/jpeg',322870,'Roof'),(6,'buddhas.jpg','image/jpeg',261152,'Buddhas'),(7,'wall.jpg','image/jpeg',369592,'Wall'),(8,'wood.jpg','image/jpeg',353050,'Wood');
+INSERT INTO `photographs` VALUES (1,'spot1.jpg','image/jpeg',265437,'spot1'),(5,'spot2.jpg','image/jpeg',394552,'spot2'),(4,'spot3.jpg','image/jpeg',322870,'spot3'),(6,'spot4.jpg','image/jpeg',261152,'spot4')/jpeg',353050,'spot4');
 
 --
 -- Table structure for table `users`
